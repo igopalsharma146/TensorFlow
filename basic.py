7a61dc1b63b41e1963c5cafd3a8c5fb450a29077
Gopal sharma <sharmagopal7316@gmail.com>
2025-04-07 02:30:34 +0530

    first commit

--- basic.py
+++ basic.py
@@ -1,1 +1,6 @@
 import tensorflow as tf
+print(tf.__version__)
+
+
+#check gpu availability
+print(tf.test.is_gpu_available())
---
+++ create constant.py
@@ -0,0 +1,14 @@
+import tensorflow as tf 
+
+#create constant
+a=tf.constant(10)
+print(a)
+
+b=tf.constant(10.2)
+print(b)
+
+c=tf.constant("10.2")
+print(c)
+
+d=tf.constant(True)
+print(d)
